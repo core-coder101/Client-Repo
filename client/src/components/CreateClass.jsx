e7042b897dad5c2160868e3ec6fbbd3d8f2a0b09
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2024-06-10 21:51:35 -0700

    createClass

--- client/src/components/CreateClass.jsx
+++ client/src/components/CreateClass.jsx
@@ -9,7 +9,12 @@ export default function CreateClass(){
   const { CSRFToken, user } = useAuth();
   const [teachers, setteachers] = useState(null);
 
-  const createTeacher = async () =>{
+    if (user.token) {
+        axios.defaults.headers.common['Authorization'] =
+        `Bearer ${user.token}`;
+    }
+
+  const GetTeachers = async () =>{
   try {
     const response = await axios.get(
         'http://127.0.0.1:8000/api/GetTeacher'
@@ -27,13 +32,72 @@ export default function CreateClass(){
     setteachers({ success: false, message: "Failed to create teacher" });
 }}
 
+
+
+
 useEffect(()=>{
   console.log(teachers);
 }, [teachers])
 
 useEffect(()=>{
-  createTeacher()
+  GetTeachers();
 },[]);
+
+
+const [result, setResult] = useState(null);
+
+
+
+const [formData, setFormData] = useState({
+  ClassName: "",
+  ClassRank: "",
+  ClassFloor: "",
+  ClassTeacherID: ""
+});
+
+const CreateClass = async (formData) => {
+  try {
+      const response = await axios.post(
+          'http://127.0.0.1:8000/api/CreateClass',
+          formData,
+          {
+              headers: {
+                  'X-CSRF-TOKEN': CSRFToken,
+                  'Content-Type': 'application/json',
+                  'API-TOKEN': 'IT is to secret you cannot break it :)',
+              },
+          }
+      );
+      setResult(response.data);
+  } catch (error) {
+      console.error(error);
+      setResult({ success: false, message: "Failed to create teacher" });
+  }
+};
+
+const [errorMessage, setErrorMessage] = useState("");
+
+useEffect(() => {
+  if (result && !result.success) {
+      setErrorMessage(result.message);
+  } else {
+      setErrorMessage("");
+  }
+}, [result]);
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+  }));
+  setErrorMessage("");
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  CreateClass(formData);
+};
 
     return(
         <div className='createClass'>
@@ -44,33 +108,38 @@ useEffect(()=>{
           </div>
         </div>
         <div className='FormBorder ms-auto me-auto'>
+        <form onSubmit={handleSubmit}>
         <div className='d-flex flex-column'>
         <label className='label'>Name of the Class</label>
-        <input className='Forminput' placeholder='Enter name of Class' name='Name' required></input>
+        <input className='Forminput' placeholder='Enter name of Class' name='ClassName' onChange={handleChange} required></input>
         </div>
         <div className='d-flex flex-column mt-3'>
         <label className='label'>Rank of the Class</label>
-        <input className='Forminput' type='number' placeholder='Enter Rank of Class' name='Rank' required></input>
+        <input className='Forminput' type='number' placeholder='Enter Rank of Class' name='ClassRank' onChange={handleChange} required></input>
         </div>
         <div className='d-flex flex-column mt-3'>
         <label className='label'>Name of the Floor</label>
-        <input className='Forminput' placeholder='Enter name of Floor' name='Floor' required></input>
+        <input className='Forminput' placeholder='Enter name of Floor' name='ClassFloor' onChange={handleChange} required></input>
         </div>
         <div className='d-flex flex-column mt-3'>
         <label className='label'>Name of the Teacher</label>
-        <select id="cars" className='Forminput' name="teachers">
+        <select id="cars" className='Forminput' name="ClassTeacherID" onChange={handleChange}>
         {teachers && Object.values(teachers).length > 0 && Object.values(teachers).map((teacherArray) => {
           if (teacherArray && teacherArray.length > 0) {
             const teacher = teacherArray[0];
-            return <option key={teacher.name} value={teacher.name}>{teacher.name}</option>;
+            return <option key={teacher.name} value={teacher.id} >{teacher.name}</option>;
           }
           return null;
         })}
         </select>
         </div>
+        {errorMessage ? <div className='errorDiv'>
+                            <p>{errorMessage}</p>
+                        </div> : null}
         <div>
-            <button className='btn btn-primary mt-3 w-100' type='button'>Submit</button>
+            <button className='btn btn-primary mt-3 w-100' type='submit'>Submit</button>
         </div>
+        </form>
         </div>
         </div>
     );
--- client/src/components/ManageClasses.jsx
+++ client/src/components/ManageClasses.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React , {useEffect,useState} from 'react'
 import "../css/table.less"
 import "../css/class.css"
 import { FaRegArrowAltCircleLeft } from "react-icons/fa";
+import { useAuth } from './context/AuthProvider';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function ManageClasses() {
+
+  const { CSRFToken, user } = useAuth();
+
+    if (user.token) {
+        axios.defaults.headers.common['Authorization'] =
+        `Bearer ${user.token}`;
+    }
+
+  const [Classes , SetClasses] = useState('');
+
+  const GetClasses = async () =>{
+    try {
+      const response = await axios.get(
+          'http://127.0.0.1:8000/api/GetClasses'
+          ,{
+              headers: {
+                  'X-CSRF-TOKEN': CSRFToken,
+                  'Content-Type': 'application/json',
+                  'API-TOKEN': 'IT is to secret you cannot break it :)',
+              },
+          }
+      );
+      SetClasses(response.data);
+  } catch (error) {
+      console.error(error);
+      SetClasses({ success: false, message: "Failed to create teacher" });
+  }}
+
+
+  useEffect(()=>{
+    GetClasses();
+  },[]);
 
 
   const navigate = useNavigate()
@@ -32,15 +66,21 @@ export default function ManageClasses() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td data-title="Rank">10 th</td>
-              <td data-title="Name">10 white House</td>
-              <td data-title="TeacherName">Ahmad Mujtaba</td>
-              <td data-title="Floor">A3</td>
+            
+            {Classes && Classes.data.map((Class) => {
+              console.log(Class);
+                return (
+              <tr>
+              <td data-title="Rank">{Class.ClassRank} th</td>
+              <td data-title="Name">{Class.ClassName}</td>
+              <td data-title="TeacherName">{Class.ClassTeacherID}</td>
+              <td data-title="Floor">{Class.ClassFloor}</td>
               <td data-title="Details"><button type="button" class="btn btn-info">Details</button></td>
               <td data-title="Edit"><button type="button" class="btn btn-warning">Edit</button></td>
               <td data-title="Delete"><button type="button" class="btn btn-danger">Delete</button></td>
             </tr>
+          );
+            })}
             <tr>
               <td data-title="Rank">10 th</td>
               <td data-title="Name">10 white House</td>
