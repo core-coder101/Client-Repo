0d22e7e8d700e56988c3f10089b69342ef1820d7
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2024-06-11 23:53:47 -0700

    studentInfo

--- client/src/components/StudentInformation.jsx
+++ client/src/components/StudentInformation.jsx
@@ -1,146 +1,225 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaRegArrowAltCircleLeft } from "react-icons/fa";
-import "../css/Teacher.css"
-import "../css/studentInformation.css"
+import "../css/Teacher.css";
+import "../css/studentInformation.css";
 import { CiSearch } from "react-icons/ci";
-import "../css/studentInformation/all.min.css"
+import "../css/studentInformation/all.min.css";
 import { IoPersonCircle } from "react-icons/io5";
 import { FaKey } from "react-icons/fa6";
 import { TiDocumentText } from "react-icons/ti";
 import { IoPerson } from "react-icons/io5";
-
+import axios from 'axios';
+import { useAuth } from './context/AuthProvider';
 
 export default function StudentInformation() {
+    const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
+    const { CSRFToken, user } = useAuth();
 
-    const navigate = useNavigate()
+    if (user.token) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${user.token}`;
+    }
 
-    const [isOpen, setIsOpen] = useState(false)
+    const [Classes, SetClasses] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [SuccessMessage, setSuccessMessage] = useState('');
 
-  return (
-    <div>
-        <div className='headingNavbar d-flex justify-content-center'>
-            <div className='d-flex'>
-                <FaRegArrowAltCircleLeft onClick={()=>{navigate("/")}} className='arrow' />
-                <h4>Dashboard \ Admit a new Student</h4>
+    const [ApiSearchData, SetApiSearchData] = useState({
+        campus: "Main Campus",
+        ClassRank: "",
+        ClassName: ""
+    });
+
+    const GetClasses = async () => {
+        try {
+            const response = await axios.get(
+                'http://127.0.0.1:8000/api/GetClasses', {
+                    headers: {
+                        'X-CSRF-TOKEN': CSRFToken,
+                        'Content-Type': 'application/json',
+                        'API-TOKEN': 'IT is to secret you cannot break it :)',
+                    },
+                }
+            );
+            SetClasses(response.data);
+            if (response.data.data && response.data.data.length > 0) {
+                const firstClass = response.data.data[0];
+                SetApiSearchData(prevData => ({
+                    ...prevData,
+                    ClassRank: firstClass.ClassRank,
+                    ClassName: firstClass.ClassName
+                }));
+            }
+        } catch (error) {
+            console.error(error);
+            setErrorMessage({ success: false, message: "Failed to Load Classes" });
+        }
+    }
+
+    const [StudentInformation, SetStudentInformation] = useState([]);
+    const GetStudentInformation = async () => {
+        try {
+            const response = await axios.post(
+                'http://127.0.0.1:8000/api/GetStudentInformation', {
+                    campus: ApiSearchData.campus,
+                    ClassRank: ApiSearchData.ClassRank,
+                    ClassName: ApiSearchData.ClassName
+                }, {
+                    headers: {
+                        'X-CSRF-TOKEN': CSRFToken,
+                        'Content-Type': 'application/json',
+                        'API-TOKEN': 'IT is to secret you cannot break it :)',
+                    },
+                }
+            );
+            SetStudentInformation(response.data.data || []);
+        } catch (error) {
+            console.error(error);
+            setErrorMessage({ success: false, message: "Failed to Get Student Info" });
+        }
+    }
+
+    useEffect(() => {
+        GetClasses();
+    }, []);
+
+    useEffect(() => {
+        if (ApiSearchData.ClassRank && ApiSearchData.ClassName) {
+            GetStudentInformation();
+        }
+    }, [ApiSearchData]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        SetApiSearchData(prev => ({
+            ...prev,
+            [name]: value,
+        }));
+        setErrorMessage("");
+        setSuccessMessage("");
+    };
+
+    return (
+        <div>
+            <div className='headingNavbar d-flex justify-content-center'>
+                <div className='d-flex'>
+                    <FaRegArrowAltCircleLeft onClick={() => { navigate("/") }} className='arrow' />
+                    <h4>Dashboard \ Admit a new Student</h4>
+                </div>
+                <div className='ms-auto me-4'></div>
             </div>
-            <div className='ms-auto me-4'></div>
+            <form>
+                <div className='inputsDiv'>
+                    <div className="inputDiv">
+                        <p>Campus</p>
+                        <select className='input' name='campus' onChange={handleChange}>
+                            <option value="Main Campus">Main Campus</option>
+                            <option value="Second Campus">Second Campus</option>
+                        </select>
+                    </div>
+                    <div className="inputDiv">
+                        <p>Class</p>
+                        <select className='input' name='ClassRank' onChange={handleChange}>
+                            {Classes.data && Classes.data.map(Class => (
+                                <option key={Class.id} value={Class.ClassRank}>{Class.ClassRank}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="inputDiv">
+                        <p>Name</p>
+                        <select className='input' name='ClassName' value={ApiSearchData.ClassName} onChange={handleChange}>
+                            {Classes.data && Classes.data.map((Class, index) => (
+                                ApiSearchData.ClassRank == Class.ClassRank && (
+                                    <option key={Class.id} value={Class.ClassName}>{Class.ClassName}</option>
+                                )
+                            ))}
+                        </select>
+                    </div>
+                    <div className="filterDataDiv">
+                        <p>Filter Data</p>
+                        <button type='button' onClick={GetStudentInformation}><CiSearch color='white' /></button>
+                    </div>
+                </div>
+            </form>
+            <div className='tableDiv'>
+                <div className="card-body">
+                    <table id="example1" className="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Roll no.</th>
+                                <th>Photo</th>
+                                <th>Name</th>
+                                <th>Parent Name</th>
+                                <th>Class</th>
+                                <th>Class Name</th>
+                                <th>Campus</th>
+                                <th>Parent Phone</th>
+                                <th>ID Card</th>
+                                <th>Reset Password</th>
+                                <th>Profile</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {StudentInformation && StudentInformation.length > 0 ? StudentInformation.map((student, index) => (
+                                <tr key={student.id}>
+                                    <td>{index + 1}</td>
+                                    <td><IoPersonCircle style={{ width: "40px", height: "40px" }} /></td>
+                                    <td>{student.name}</td>
+                                    <td>{student.parentName}</td>
+                                    <td>{student.classRank}</td>
+                                    <td>{student.className}</td>
+                                    <td>{student.campus}</td>
+                                    <td>{student.parentPhone}</td>
+                                    <td>
+                                        <div className="filterDataDiv generateID innerButtonDiv">
+                                            <p>Generate ID</p>
+                                            <button><TiDocumentText color='white' style={{ width: "18px", height: "18px" }} /></button>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className="filterDataDiv resetPassword innerButtonDiv">
+                                            <p>Reset Password</p>
+                                            <button><FaKey color='white' style={{ width: "18px", height: "18px" }} /></button>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className="filterDataDiv viewProfile innerButtonDiv">
+                                            <p>View Profile</p>
+                                            <button><IoPerson color='white' style={{ width: "18px", height: "18px" }} /></button>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className="dropdown">
+                                            <button
+                                                className="dropdown-toggle customButton"
+                                                type="button"
+                                                id="dropdownMenuButton"
+                                                data-toggle="dropdown"
+                                                aria-haspopup="true"
+                                                aria-expanded={isOpen}
+                                                onClick={() => { setIsOpen(prev => !prev) }}
+                                            >
+                                                Actions
+                                            </button>
+                                            <div className={`customDropDown dropdown-menu${isOpen ? ' show' : ''}`} aria-labelledby="dropdownMenuButton">
+                                                <a className="dropdown-item" href="#">Edit</a>
+                                                <a className="dropdown-item" href="#">Delete</a>
+                                                <a className="dropdown-item" href="#">Deactivate Student</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            )) : (
+                                <tr>
+                                    <td colSpan="12" className="text-center">No Student Information Available</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-        <form>
-            <div className='inputsDiv'>
-                <div className="inputDiv">
-                    <p>Campus</p>
-                    <select className='input' name='campus'>
-                        <option value={"Main Campus"} >Main Campus</option>
-                        <option value={"SecondCampus"} >SecondCampus</option>
-                    </select>
-                </div>
-                <div className="inputDiv">
-                    <p>Class</p>
-                    <select className='input' name='class'>
-                        <option value={1} >1</option>
-                        <option value={2} >2</option>
-                        <option value={3} >3</option>
-                        <option value={4} >4</option>
-                        <option value={5} >5</option>
-                        <option value={6} >6</option>
-                        <option value={7} >7</option>
-                        <option value={8} >8</option>
-                        <option value={9} >9</option>
-                        <option value={10} >10</option>
-                        <option value={2} >11</option>
-                        <option value={2} >12</option>
-                    </select>
-                </div>
-                <div className="inputDiv">
-                    <p>Name</p>
-                    <select className='input' name='name'>
-                        <option value={"Green House"} >Green House</option>
-                        <option value={"White House"} >White House</option>
-                    </select>
-                </div>
-                <div className="filterDataDiv">
-                    <p>Filter Data</p>
-                    <button type='submit'><CiSearch color='white    ' /></button>
-                </div>
-            </div>
-        </form>
-        <div className='tableDiv'>
-            <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                    <thead>
-                    <tr>
-                    <th>Roll no.</th>
-                    <th>Photo</th>
-                    <th>Name</th>
-                    <th>Parent Name</th>
-                    <th>Class</th>
-                    <th>Class Name</th>
-                    <th>Campus</th>
-                    <th>Parent Phone</th>
-                    <th>ID Card</th>
-                    <th>Reset Password</th>
-                    <th>Profile</th>
-                    <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td><IoPersonCircle style={{width: "40px", height: "40px"}} /></td>
-                        <td>Mohsin Jawad</td>
-                        <td>Jawad Saleem</td>
-                        <td>11</td>
-                        <td>P6</td>
-                        <td>Commerce</td>
-                        <td>03033050736</td>
-                        <td>
-                            <div className="filterDataDiv generateID innerButtonDiv">
-                                <p>Generate ID</p>
-                                <button ><TiDocumentText color='white' style={{width: "18px", height: "18px"}} /></button>
-                            </div>
-                        </td>
-                        <td>
-                            <div className="filterDataDiv resetPassword innerButtonDiv">
-                                <p>Reset Password</p>
-                                <button><FaKey color='white' style={{width: "18px", height: "18px"}} /></button>
-                            </div>
-                        </td>
-                        <td>
-                            <div className="filterDataDiv viewProfile innerButtonDiv">
-                                <p>View Profile</p>
-                                <button><IoPerson color='white' style={{width: "18px", height: "18px"}} /></button>
-                            </div>
-                        </td>
-                            <div className="dropdown">
-                                <button
-                                    className="dropdown-toggle customButton"
-                                    type="button"
-                                    id="dropdownMenuButton"
-                                    data-toggle="dropdown"
-                                    aria-haspopup= "true"
-                                    aria-expanded={isOpen}
-                                    onClick={()=>{setIsOpen(prev => !prev)}}
-                                >
-                                    Actions
-                                </button>
-                                <div className={`customDropDown dropdown-menu${isOpen ? ' show' : ''}`} aria-labelledby="dropdownMenuButton">
-                                    <a className="dropdown-item" href="#">
-                                    Edit
-                                    </a>
-                                    <a className="dropdown-item" href="#">
-                                    Delete
-                                    </a>
-                                    <a className="dropdown-item" href="#">
-                                    Deactivate Student
-                                    </a>
-                                </div>
-                            </div>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-  )
+    );
 }
