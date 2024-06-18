6d46b006bf253917b39d51bdea8f1f588a55875b
core-coder101 <mohsinjawad86@gmail.com>
2024-06-18 23:17:24 +0500

    generate id working

--- client/src/components/StudentInformation.jsx
+++ client/src/components/StudentInformation.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaRegArrowAltCircleLeft } from "react-icons/fa";
 import "../css/Teacher.css";
@@ -13,8 +13,13 @@ import { useAuth } from './context/AuthProvider';
 import defaultImg from "../img/default.png"
 import Popup from "react-animated-popup"
 import Preloader from './Preloader';
-import { Tooltip } from '@mui/material';
-
+import { Button, Tooltip } from '@mui/material';
+import { ReactBarcode } from "react-jsbarcode"
+import { FaLocationDot } from "react-icons/fa6";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { IoMail } from "react-icons/io5";
+import { toPng } from 'html-to-image';
+import converter from 'number-to-words'
 
 
 export default function StudentInformation() {
@@ -22,6 +27,8 @@ export default function StudentInformation() {
     const navigate = useNavigate();
 
     // moved all state definitions to the top (hope it doesn't break anything when merging)
+
+    const pngElementRef = useRef(null)
     
     const [isOpen, setIsOpen] = useState({});
     const [Classes, SetClasses] = useState([]);
@@ -31,6 +38,10 @@ export default function StudentInformation() {
     const [filteredStudentsInfo, setFilteredStudentsInfo] = useState([])
     const [search, setSearch] = useState("")
     const [loading, setLoading] = useState(false)
+    const [idPopup, setIdPopup] = useState(false)
+    const [passwordPopup, setPasswordPopup] = useState(false)
+    const [profilePopup, setProfilePopup] = useState(false)
+    const [popupInput, setPopupInput] = useState(null)
     const [ApiSearchData, SetApiSearchData] = useState({
         campus: "Main Campus",
         ClassRank: "",
@@ -51,6 +62,8 @@ export default function StudentInformation() {
     }
 
     const GetClasses = async () => {
+        setErrorMessage("Loading Class data")
+        setLoading(true)
         try {
             const response = await axios.get(
                 'http://127.0.0.1:8000/api/GetClasses', {
@@ -76,8 +89,7 @@ export default function StudentInformation() {
     }
 
     const GetStudentInformation = async () => {
-        setErrorMessage("Loading Students' Information")
-        setLoading(true)
+        setErrorMessage("Loading Students' data")
         try {
             const response = await axios.post(
                 'http://127.0.0.1:8000/api/GetStudentInformation', {
@@ -185,6 +197,37 @@ export default function StudentInformation() {
     }, [search, StudentInformation]);
 
 
+    const generatePopup = (input, type)=>{
+        // the idea is that this function works for all three of the buttons: generate id, reset password and view profile
+        setPopupInput(input)
+        const setTypePopup = {
+            id: setIdPopup,
+            password: setPasswordPopup,
+            profie: setProfilePopup,
+        }
+
+        if (setTypePopup[type]) {
+            setTypePopup[type](true);
+        }
+    }
+
+    const htmlToPng = async (name) => {
+        setErrorMessage("Generating image. . .")
+        setLoading(true)
+        try{
+            let dataUrl = await toPng(pngElementRef.current, { cacheBust: false })
+            const link = document.createElement("a");
+            link.download = name + ".png";
+            link.href = dataUrl;
+            link.click();
+        } catch(err){
+            console.log(err.message);
+        } finally {
+            setLoading(false)
+        }
+    }
+
+
     return (
         <div>
             <div className='headingNavbar d-flex justify-content-center'>
@@ -266,7 +309,7 @@ export default function StudentInformation() {
                                     <td>{ApiSearchData.campus}</td>
                                     <td>{student.parents.GuardiansPhoneNumber}</td>
                                     <td>
-                                        <div className="filterDataDiv generateID innerButtonDiv">
+                                        <div onClick={()=>{generatePopup(student, 'id')}} className="filterDataDiv generateID innerButtonDiv">
                                             <p>Generate ID</p>
                                             <button><TiDocumentText color='white' style={{ width: "18px", height: "18px" }} /></button>
                                         </div>
@@ -312,6 +355,31 @@ export default function StudentInformation() {
                                     <td colSpan="12" className="text-center">No Student Information Available</td>
                                 </tr>
                             )}
+                            {popupInput && <Popup visible={idPopup} animationDuration={400} onClose={() => {setIdPopup(false); setTimeout(()=>{setPopupInput(null)}, 400)}} style={{backgroundColor: "#11101de9", boxShadow: "rgba(0, 0, 0, 0.2) 5px 5px 5px 5px", padding: "40px 20px", width: "700px", height: "600px", display: "flex", alignItems: "center", flexDirection: "column"}}>
+                                <div ref={pngElementRef} className='studentIdCardDiv' style={{backgroundColor: "white", display: "flex", flexDirection: "column", alignItems: "center", borderRadius: "1rem", padding: "15px 20px", boxShadow: "rgba(0, 0, 0, 0.5) 5px 5px 5px 5px", whiteSpace: "nowrap", width: "300px", height: "460px"}}>
+                                    <div style={{width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
+                                        <h4 style={{margin: "0", padding: "0"}}>{popupInput.users.name}</h4>
+                                        <ReactBarcode style={{backgroundColor: "transparent"}} value={ApiSearchData.campus.slice(0, 1) + '-' + JSON.stringify(popupInput.id)} options={{format: "code128"}} displayValue={false} renderer='image' />
+                                        <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
+                                            <h6 style={{fontSize: "14px"}}><span style={{color: "#5b8beb"}}>Class: </span>{converter.toWords(ApiSearchData.ClassRank)}</h6>
+                                            <h6 style={{fontSize: "14px"}}><span style={{color: "#5b8beb"}}>Campus: </span>{ApiSearchData.campus}</h6>
+                                            <h6 style={{fontSize: "14px"}}><span style={{color: "#5b8beb"}}>Student ID: </span>{popupInput.id}</h6>
+                                            <h6 style={{fontSize: "14px"}}><span style={{color: "#5b8beb"}}>Full Name: </span>{popupInput.users.name}</h6>
+                                            <h6 style={{ paddingBottom: "50px", fontSize: "14px"}}><span style={{color: "#5b8beb",}}>Emergency Contact: </span>{popupInput.parents.GuardiansPhoneNumber}</h6>
+                                        </div>
+                                        <div style={{display: "flex", width: "100%"}}>
+                                            <FaLocationDot style={{marginRight: "5px"}} color="#5b8beb" /><h6 style={{fontSize: "14px"}}><span style={{color: "#5b8beb"}}>Faisalabad, Kohinoor City</span></h6>
+                                        </div>
+                                        <div style={{display: "flex", width: "100%"}}>
+                                            <BsFillTelephoneFill style={{marginRight: "5px"}} color="#5b8beb" /><h6 style={{fontSize: "14px"}}><span style={{color: "#5b8beb"}}>+921234567890</span></h6>
+                                        </div>
+                                        <div style={{display: "flex", width: "100%"}}>
+                                            <IoMail style={{marginRight: "5px"}} color="#5b8beb" /><h6 style={{fontSize: "14px"}}><span style={{color: "#5b8beb"}}>HustlersUniversity@gmail.com</span></h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <Button onClick={()=>{htmlToPng(popupInput.users.name)}} style={{marginTop: "20px"}} variant="contained" color="primary">Download</Button>
+                            </Popup>}
                             <Popup visible={popup} onClose={() => {setPopup(false); setTimeout(()=>{setErrorMessage("")},400)}}  style={{backgroundColor: "#11101de9", boxShadow: "rgba(0, 0, 0, 0.2) 5px 5px 5px 5px", padding: "40px 20px"}}>
                                 <div className='d-flex justify-content-center align-items-center' style={{width: "max-content", height: "100%", padding: "0"}}>
                                     <h5 style={{color: "white", margin: "0"}}>{errorMessage}</h5>
