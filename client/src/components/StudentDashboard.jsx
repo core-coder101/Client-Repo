44f00721240264accf7badc3b5f7e1aa8bf566c0
core-coder101 <mohsinjawad86@gmail.com>
2024-06-21 17:05:10 +0500

    student dashboard 2.0

---
+++ client/src/components/StudentDashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function StudentDashboard() {
+  return (
+    <div className='headingNavbar d-flex justify-content-center'>
+            <div className='d-flex'><h4>Dashboard</h4></div>
+            <div className='ms-auto me-4'></div>
+          </div>
+  )
+}
