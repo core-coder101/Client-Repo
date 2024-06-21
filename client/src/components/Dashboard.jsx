3cd51fa4607cc2bf81b0d668bd01e5f3ec7751c2
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2024-06-21 05:06:26 -0700

    nothing much

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -8,12 +8,45 @@ import { FaChartPie } from "react-icons/fa";
 import { BsGraphUpArrow } from "react-icons/bs";
 import { VscGraphLine } from "react-icons/vsc";
 import Graph from './Graph';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+
+import { PieChart } from '@mui/x-charts/PieChart';
 
 
+
+
+const data1 = [
+  { label: 'Present Staff', value: 20 }
+  ];
+
+const data2 = [
+  { label: 'Present', value: 20},
+  { label: 'Absent', value: 10 },
+];
+
+
+const series = [
+  
+  {
+    innerRadius: 0,
+    outerRadius: 80,
+    id: 'series-1',
+    data: data1,
+    arcLabel: (text) => `Present Staff`,
+    arcLabelMinAngle: 45,
+  },
+  {
+    innerRadius: 110,
+    outerRadius: 140,
+    id: 'series-2',
+    data: data2,
+  },
+];
 
 export default function Dashboard() {
-
-
 
   const cardBackgroundIconStyles = { opacity: "20%",
     width: "80%",
@@ -21,6 +54,8 @@ export default function Dashboard() {
     position: "absolute",
     right: "-40px",
   }
+  const [itemData, setItemData] = useState();
+
 
   return (
     <div className='dashboard'>
@@ -68,8 +103,53 @@ export default function Dashboard() {
             <button style={{backgroundColor: "#C84332"}}>More Info <FaArrowCircleRight /></button>
           </div>
         </div>
-        <Graph />
-        
+        <div className='d-flex' style={{marginTop:"70px"}}>
+        <div className='ms-auto me-auto'>
+        <center><h2 className='protest-revolution-regular mb-4'>Fee Generated and Submitted</h2></center>
+        <Graph className="mt-1" />
+        </div>
+      <div>
+      <h2 className='protest-revolution-regular mb-4'>Today Present Staff</h2>
+        <Stack
+      direction={{ xs: 'column', md: 'row' }}
+      spacing={{ xs: 0, md: 4 }}
+      sx={{ width: '100%' }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <PieChart
+          colors={['#03a459','#dc493b']}
+          series={series}
+          
+          width={400}
+          height={300}
+          slotProps={{
+            legend: { hidden: true },
+          }}
+          onItemClick={(event, d) => setItemData(d)}
+        />{' '}
+      </Box>
+
+      <Stack direction="column" sx={{ width: { xs: '100%', md: '40%' } }}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <IconButton
+            aria-label="reset"
+            size="small"
+            onClick={() => {
+              setItemData(null);
+            }}
+          >
+          </IconButton>
+        </Box>
+      </Stack>
+    </Stack>
+        </div>
+        </div>
     </div>
   )
 }
