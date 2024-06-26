0cf0981bfe815abb2d582340bec430df695be41d
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2024-06-26 05:57:08 -0700

    watchvideo

--- client/src/components/Admin/WatchVideoes.jsx
+++ client/src/components/Admin/WatchVideoes.jsx
@@ -8,13 +8,14 @@ export default function WatchVideoes() {
       <div className='row m-0 p-0'>
         <div className='col-8 videoSideDiv'>
           <div className='videodiv'>
-          <video className='video' controls autoPlay>
-            <source src={vid2} poster='https://ik.imagekit.io/ikmedia/example_video.mp4/ik-thumbnail.jpg?tr=w-1200,h-680' type="video/mp4" />
+          <video className='video' controls >
+            <source src={vid2} autoPlay poster='https://ik.imagekit.io/ikmedia/example_video.mp4/ik-thumbnail.jpg?tr=w-1200,h-680' type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           </div>
-          <div>
-            <h4>Build a Realtime Chat App with React</h4>
+          <div className='video-Details'>
+            <h4 className='Video-Title roboto-black-italic'>Build a Realtime Chat App with React not support the video tag. hiihi </h4>
+            
           </div>
         </div>
         <div className='col-4'>
