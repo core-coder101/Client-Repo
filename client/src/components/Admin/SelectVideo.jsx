1cfac67264478745cfa3da21b2645a026bd0b450
Ahmad Mujtaba Saeed <ahmadmujtabap70@gmail.com>
2024-07-01 04:35:23 -0700

    watch videos frontend integration

--- client/src/components/Admin/SelectVideo.jsx
+++ client/src/components/Admin/SelectVideo.jsx
@@ -4,20 +4,23 @@ import { RiPlayList2Fill } from "react-icons/ri";
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import DefaultImg from "../../assets/img/default.png";
+import { useNavigate } from 'react-router-dom';
 
 
 export default function SelectVideo() {
 
+  const navigate = useNavigate();
+
 
   const { CSRFToken} = useSelector((state) => state.auth)
 
-  const [Subject,setSubject] = useState('Not in Categories');
+  const [Subject,setSubject] = useState('General');
   const [Rank, SetRank] = useState(10);
   const [VideosData , SetVideosData] = useState();
   const [PlaylistData , SetPlaylistData] = useState();
 
   const names = [
-    "Not in Categories",
+    "General",
     "Maths",
     "General Science",
     "Physics",
@@ -44,8 +47,10 @@ export default function SelectVideo() {
       );
       if(response.data.message == 'video'){
         SetVideosData(response.data.data);
+        SetPlaylistData([]);
       }
       else{
+        SetVideosData([]);
         SetPlaylistData(response.data.data);
       }
   } catch (error) {
@@ -54,6 +59,8 @@ export default function SelectVideo() {
   }
 
   useEffect(()=>{
+    SetVideosData([]);
+    SetPlaylistData([]);
     GetVideoData();
   },[Subject])
 
@@ -71,36 +78,38 @@ export default function SelectVideo() {
     </div>
     <div class="section">
       <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xxl-4 g-4 custom-card1">
-      { PlaylistData && PlaylistData.length > 0 && PlaylistData.map((PlaylistData,index)=>{
-        return(
-        <button onClick={()=>{}} className='outerCardBtn'>
+      { PlaylistData && PlaylistData.length > 0 && PlaylistData.map((Data,index)=>{
+         return (
+          (Data.videos.length > 0) ?
+        <button onClick={()=>{navigate(`/watchvideo/${Data.videos[0].id}`)}} className='outerCardBtn'>
         <div class="col ">
           <div class="card custom-card playlist">
             <div className='Cardimage'><img src={
-                            PlaylistData.videos[0].images
-                              ? `data:image/png;base64,${PlaylistData.videos[0].images.data}`
+                            Data.videos[0].images
+                              ? `data:image/png;base64,${Data.videos[0].images.data}`
                               : ''
                           } class="card-img-top custom-img" alt="..." />
             <div className='playlisticon'><RiPlayList2Fill/></div>
             </div>
             <img class="channelimg" src={
-                            PlaylistData.users.images
-                              ? `data:image/png;base64,${PlaylistData.users.images.data}`
+                            (Data.users.images.data)
+                              ? `data:image/png;base64,${Data.users.images.data}`
                               : DefaultImg
                           } width="30px" />
             <div class="card-body costom-body">
-              <h5 class="card-title customtitle">{PlaylistData.PlaylistTitle}</h5>
-              <p class="card-text customchannelname">{PlaylistData.users.name}</p>
-              <p class="card-text customviews">342 veiws ,{PlaylistData.Date}</p>
+              <h5 class="card-title customtitle">{Data.PlaylistTitle}</h5>
+              <p class="card-text customchannelname">{Data.users.name}</p>
+              <p class="card-text customviews">342 veiws ,{Data.Date}</p>
             </div>
           </div>
         </div>
         </button>
+        : "" 
         )
       })}
       {VideosData && VideosData.length > 0 && VideosData.map((videoData)=>{
         return(
-          <button onClick={()=>{}} className='outerCardBtn'>
+          <button onClick={()=>{navigate(`/watchvideo/${videoData.id}`)}} className='outerCardBtn'>
         <div class="col">
           <div class="card custom-card">
           <div className='Cardimage'>
@@ -128,85 +137,6 @@ export default function SelectVideo() {
         );
       })}
      
-        <div class="col blocking3">
-          <div class="card custom-card">
-            <img src="images/f16istoogood.PNG" class="card-img-top custom-img" alt="..." />
-            <img class="channelimg" src="images/channels4_profile (3).jpg" width="30px" />
-            <div class="card-body costom-body">
-              <h5 class="card-title customtitle">F-16C Experience.mp4</h5>
-              <p class="card-text customchannelname">Malzi</p>
-              <p class="card-text customviews">890 veiws ,12 days ago</p>
-            </div>
-          </div>
-        </div>
-        <div class="col blocking4">
-          <div class="card custom-card">
-            <img src="images/neverlieaway.PNG" class="card-img-top custom-img" alt="..." />
-            <img class="channelimg" src="images/unnamed (2).jpg" width="30px" />
-            <div class="card-body costom-body">
-              <h5 class="card-title customtitle">Never lie away perfectly reverb and slowed</h5>
-              <p class="card-text customchannelname">DopeLine</p>
-              <p class="card-text customviews">1.2M veiws ,2 years ago</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card custom-card">
-            <img src="images/plane.PNG" class="card-img-top custom-img" alt="..." />
-            <img class="channelimg" src="images/channels4_profile (4).jpg" width="30px" />
-            <div class="card-body costom-body">
-              <h5 class="card-title customtitle">NUCLEAR BOMBERS in War Thunder!</h5>
-              <p class="card-text customchannelname">Vadact</p>
-              <p class="card-text customviews">2.6k veiws ,23 Hours ago</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card custom-card">
-            <img src="images/plane.PNG" class="card-img-top custom-img" alt="..." />
-            <img class="channelimg" src="images/channels4_profile (4).jpg" width="30px" />
-            <div class="card-body costom-body">
-              <h5 class="card-title customtitle">NUCLEAR BOMBERS in War Thunder!</h5>
-              <p class="card-text customchannelname">Vadact</p>
-              <p class="card-text customviews">2.6k veiws ,23 Hours ago</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="col">
-          <div class="card custom-card">
-            <img src="images/plane.PNG" class="card-img-top custom-img" alt="..." />
-            <img class="channelimg" src="images/channels4_profile (4).jpg" width="30px" />
-            <div class="card-body costom-body">
-              <h5 class="card-title customtitle">NUCLEAR BOMBERS in War Thunder!</h5>
-              <p class="card-text customchannelname">Vadact</p>
-              <p class="card-text customviews">2.6k veiws ,23 Hours ago</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card custom-card">
-            <img src="images/plane.PNG" class="card-img-top custom-img" alt="..." />
-            <img class="channelimg" src="images/channels4_profile (4).jpg" width="30px" />
-            <div class="card-body costom-body">
-              <h5 class="card-title customtitle">NUCLEAR BOMBERS in War Thunder!</h5>
-              <p class="card-text customchannelname">Vadact</p>
-              <p class="card-text customviews">2.6k veiws ,23 Hours ago</p>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card custom-card">
-            <img src="images/music1.PNG" class="card-img-top custom-img" alt="..." />
-            <img class="channelimg" src="images/channels4_profile (5).jpg" width="30px" />
-            <div class="card-body costom-body">
-              <h5 class="card-title customtitle">Night Lofi Songs | Mind relaxing songs | slowed</h5>
-              <p class="card-text customchannelname">Glam Music</p>
-              <p class="card-text customviews">2M veiws ,3 years ago</p>
-            </div>
-          </div>
-        </div>
-
       </div>
       </div>
     </>
--- client/src/components/Admin/WatchVideos.jsx
+++ client/src/components/Admin/WatchVideos.jsx
@@ -6,17 +6,78 @@ import { getVideoByID, setError, setPopup } from '../../redux/slices/WatchVideos
 import CustomPopup from '../common/CustomPopup';
 import Comment from './Comment';
 import PlaylistItem from './PlaylistItem';
+import axios from 'axios';
 
 export default function WatchVideoes() {
   const { ID } = useParams();
-  const { loading, error, popup, videoInfo, file } = useSelector(state => state.watchVideos);
+  const { loading, error, popup, videoInfo, file  } = useSelector(state => state.watchVideos);
+  
+  const { CSRFToken } = useSelector((state) => state.auth)
+
   const dispatch = useDispatch();
 
   const descriptionText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
   const [showMore, setShowMore] = useState(false)
 
+  const [PlaylistData,SetPlaylistData] = useState([]);
+
+  const [comment , setcomment] = useState('');
+
+
+  const GetplaylistData = async () =>{
+    try {
+      const response = await axios.get(
+          `http://127.0.0.1:8000/api/GetplaylistData?PlaylistID=${videoInfo.VideoPlaylistID}`,
+          {
+              headers: {
+                  'X-CSRF-TOKEN': CSRFToken,
+                  'Content-Type': 'application/json',
+                  'API-TOKEN': 'IT is to secret you cannot break it :)',
+              },
+          }
+      );
+        SetPlaylistData(response.data.data);
+  } catch (error) {
+
+  } 
+  }
+
+
+
+
+
+  const SubmitComment = async () =>{
+    try {
+      const response = await axios.post(
+          `http://127.0.0.1:8000/api/UploadComment`,{
+            VideoID : ID,
+            Comment : comment
+          },
+          {
+              headers: {
+                  'X-CSRF-TOKEN': CSRFToken,
+                  'Content-Type': 'application/json',
+                  'API-TOKEN': 'IT is to secret you cannot break it :)',
+              },
+          }
+      );
+        // SetPlaylistData(response.data.data);
+  } catch (error) {
+
+  } 
+  }
+
+
+
+
+
   useEffect(() => {
+    if(videoInfo){
+      if(videoInfo.VideoPlaylistID != null){
+        GetplaylistData()
+      }
+    }
     if (ID) {
       dispatch(getVideoByID(ID))
     }
@@ -27,6 +88,17 @@ export default function WatchVideoes() {
     useEffect(()=>{
       setLocalLoading(loading)
     }, [loading])
+
+
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+          event.preventDefault(); // Prevent the default form submission behavior
+          SubmitComment();
+      }
+  };
+
+
 
 
   return (
@@ -43,35 +115,38 @@ export default function WatchVideoes() {
         <div className='playlistItems'>
             <div className='fixedTopDiv'>
                 <div className='info'>
-                <h6>g3ox_em - GigaChad Theme (Phonk House Version)</h6>
-                <p>g3ox_em - 1 / 10</p>
+                <h6>{PlaylistData.PlaylistTitle}</h6>
+                <p>{PlaylistData.PlaylistCategory} - 1 / </p>
+                {/* {(PlaylistData.videos)} */}
                 </div>
             </div>
             <div className='overflowDiv' style={{width: "100%", overflowY: "auto", zIndex: "0"}}>
-                <PlaylistItem />
-                <PlaylistItem />
-                <PlaylistItem />
-                <PlaylistItem />
-                <PlaylistItem />
-                <PlaylistItem />
-                <PlaylistItem />
-                <PlaylistItem />
-                <PlaylistItem />
+                {PlaylistData && PlaylistData.videos.map((video,index)=>{
+                  return (<PlaylistItem 
+                  index={index}
+                  key = {video.id}
+                  Title = {video.VideoTitle}
+                  VideoLength = {video.VideoLength}
+                  image = {video.images.data}
+                  UName = "Ahmad"
+                  />);
+                })}
+                
             </div>
         </div>
     </div>
           <div className='video-Details'>
-          <h4 className='Video-Title roboto-black-italic'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4>
+          <h4 className='Video-Title roboto-black-italic'>{videoInfo ? videoInfo.VideoTitle :""}</h4>
 
             <div className='description'>
-              {showMore ? descriptionText : descriptionText.substring(0, 100)}
+              {showMore ? (videoInfo ? videoInfo.VideoDescription: "") : (videoInfo ? videoInfo.VideoDescription.substring(0, 100): "")}
               {/* <br /> */}
               <button className='morebutton text-info' onClick={()=>{setShowMore(prev=>!prev)}}>{showMore ? " . . .show less" : " . . .show more"}</button>
             </div>
             <hr />
             <div>
-              <h5 className='comments_num'>13 Comments</h5>
-              <input name='Comment' className='commentinput mb-3' placeholder='Add a comment' />
+              <h5 className='comments_num'>13 Comments {comment}</h5>
+              <input name='comment' onKeyDown={handleKeyDown} id='comment' onChange={(e)=>{setcomment(e.target.value)}} className='commentinput mb-3' placeholder='Add a comment' />
               <Comment />
             </div>
           </div>
